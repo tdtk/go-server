c75965d7461ee7b80f369034fe7c250666c494b8
JP26592 <taku.tada@linecorp.com>
2020-07-31 14:02:50 +0900

    feat: impl crud

--- docker/mysql/initdb.d/init.sql
+++ docker/mysql/initdb.d/init.sql
@@ -18,36 +18,37 @@ CREATE TABLE user_info
   user_name varchar(50),
   telephone varchar(50),
   password varchar(50),
-  role_id int
+  role_id int,
+  is_deleted boolean
 );
 
 insert into user_info
 values
-  (null, "sato", "佐藤", "0901111111", "axiz", 1);
+  (null, "sato", "佐藤", "0901111111", "axiz", 1, 0);
 insert into user_info
 values
-  (null, "suzuki", "鈴木", "09022222222", "bxiz", 1);
+  (null, "suzuki", "鈴木", "09022222222", "bxiz", 1, 0);
 insert into user_info
 values
-  (null, "takahashi", "高橋", "09033333333", "cxiz", 1);
+  (null, "takahashi", "高橋", "09033333333", "cxiz", 1, 0);
 insert into user_info 
 values
-  (null, "tanaka", "田中", "09044444444", "dxiz", 2);
+  (null, "tanaka", "田中", "09044444444", "dxiz", 2, 0);
 insert into user_info 
 values
-  (null, "ito", "伊藤", "09055555555", "exiz", 2);
+  (null, "ito", "伊藤", "09055555555", "exiz", 2, 0);
 insert into user_info 
 values
-  (null, "yamamoto", "山本", "09066666666", "fxiz", 2);
+  (null, "yamamoto", "山本", "09066666666", "fxiz", 2, 0);
 insert into user_info 
 values
-  (null, "watanabe", "渡辺", "09077777777", "gxiz", 2);
+  (null, "watanabe", "渡辺", "09077777777", "gxiz", 2, 0);
 insert into user_info 
 values
-  (null, "nakamura", "中村", "09088888888", "hxiz", 2);
+  (null, "nakamura", "中村", "09088888888", "hxiz", 2, 0);
 insert into user_info 
 values
-  (null, "kobayashi", "小林", "09099999999", "ixiz", 2);
+  (null, "kobayashi", "小林", "09099999999", "ixiz", 2, 0);
 insert into user_info 
 values
-  (null, "kato", "加藤", "09000000000", "jxiz", 2);
+  (null, "kato", "加藤", "09000000000", "jxiz", 2, 0);
--- repository/user_repo.go
+++ repository/user_repo.go
@@ -49,9 +49,85 @@ func (repo *UserRepository) FindAllUser() []model.UserInfo {
 	return users
 }
 
+// SearchUser is ...
+func (repo *UserRepository) SearchUser(userName string, telephone string) []model.UserInfo {
+
+	var query string
+
+	var selectAll = "select user_id, login_id, user_name, telephone, password, role_id" +
+		"from user_info" +
+		"where is_deleted = 0"
+
+	if userName != "" && telephone != "" {
+		query = fmt.Sprintf(
+			selectAll+
+				"and user_name like '%%%s%%'"+
+				"and telephone like '%%%s%%'",
+			userName,
+			telephone,
+		)
+	} else if userName != "" {
+		query = fmt.Sprintf(
+			selectAll+
+				"and user_name like '%%%s%%'",
+			userName,
+		)
+	} else if telephone != "" {
+		query = fmt.Sprintf(
+			selectAll+
+				"and telephone like '%%%s%%'",
+			telephone,
+		)
+	} else {
+		query = selectAll
+	}
+
+	results, err := repo.db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var users []model.UserInfo
+
+	for results.Next() {
+		var user model.UserInfo
+		err = results.Scan(&user.UserID, &user.LoginID, &user.UserName, &user.Telephone, &user.Password, &user.RoleID)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		users = append(users, user)
+	}
+
+	return users
+}
+
+// GetUserByID is ...
+func (repo *UserRepository) GetUserByID(userID int) model.UserInfo {
+	results, err := repo.db.Query(fmt.Sprintf("select user_id, login_id, user_name, telephone, password, role_id from user_info where user_id=%d", userID))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var user model.UserInfo
+		err = results.Scan(&user.UserID, &user.LoginID, &user.UserName, &user.Telephone, &user.Password, &user.RoleID)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		return user
+	}
+	panic("Can't get user!")
+}
+
 // GetPasswordByID is ...
 func (repo *UserRepository) GetPasswordByID(loginID string) string {
-	results, err := repo.db.Query(fmt.Sprintf("select password from user_info where login_id='%s'", loginID))
+	results, err := repo.db.Query(fmt.Sprintf("select password from user_info where login_id='%s' and is_deleted = 0", loginID))
 
 	if err != nil {
 		panic(err.Error())
@@ -68,6 +144,82 @@ func (repo *UserRepository) GetPasswordByID(loginID string) string {
 		return pass
 	}
 	panic("Can't get password!")
+}
+
+// UpdateUser is ...
+func (repo *UserRepository) UpdateUser(user model.UserInfo) {
+	_, err := repo.db.Query(
+		fmt.Sprintf(
+			"update user_info"+
+				"set login_id = %s"+
+				"set user_name = '%s'"+
+				"set telephone = '%s'"+
+				"set password = '%s'"+
+				"set role_id = %d"+
+				"where id = %d",
+			user.LoginID,
+			user.UserName,
+			user.Telephone,
+			user.Password,
+			user.RoleID,
+			user.UserID,
+		),
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+// DeleteUser is ...
+func (repo *UserRepository) DeleteUser() {
+	_, err := repo.db.Query("update user_info set is_deleted = 1")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+// GetRoleByID is ...
+func (repo *UserRepository) GetRoleByID(roleID string) model.Role {
+	results, err := repo.db.Query(fmt.Sprintf("select * from role where role_id='%s'", roleID))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var role model.Role
+		err = results.Scan(&role.RoleID, &role.RoleName)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		return role
+	}
+	panic("Can't get role!")
+}
+
+// GetAllRole is ...
+func (repo *UserRepository) GetAllRole() []model.Role {
+	results, err := repo.db.Query("select * from role")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var roles []model.Role
+
+	for results.Next() {
+		var role model.Role
+		err = results.Scan(&role.RoleID, &role.RoleName)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		roles = append(roles, role)
+	}
+
+	return roles
 }
 
 // Close is ...
